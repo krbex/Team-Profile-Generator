2feef8560a716bcd24442a37a7bc2bb237f5377f
krbex <aidan.krbec5@gmail.com>
2022-10-11 20:18:53 -0500

    rewrote generation file, now forEach is not functional

--- src/generateMockup.js
+++ src/generateMockup.js
@@ -1,106 +1,69 @@
 const fs = require('fs')
 
-function generateTeam(employeeArray) {
+var pageText = `
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Team Profile</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-    function generateManager(manager) {
-        return `
-        <div class = 'card'>
-            <div class = 'header'>
-                <h2> ${manager.getName()}</h2>
-                <h3><i class="fa-sharp fa-solid fa-paperclip"></i> Manager</h3>
-            </div>
-            <div>
-                <ul>
-                    <li id = 'employeeId'>${manager.getId()}</li>
-                    <li id = 'managerEmail'>Email: <a href='mailto:${manager.getEmail()}'>${manager.getEmail()}</a></li>
-                    <li> Office number: ${manager.getOfficeNumber()}</li>
-                </ul>
-            </div>
-        </div>
-        `;
-    }
 
-    function generateEngineer(engineer) {
-        return `
-        <div class = 'card'>
-            <div class = 'header'>
-                <h2> ${engineer.getName()}</h2>
-                <h3><i class="fa-sharp fa-solid fa-wrench-simple"></i> Engineer</h3>
-            </div>
-            <div>
-                <ul>
-                    <li id = 'employeeId'>${engineer.getId()}</li>
-                    <li id = 'managerEmail'>Email: <a href='mailto:${engineer.getEmail()}'>${engineer.getEmail()}</a></li>
-                    <li> GitHub: <a href='https://github.com/${engineer.getGithub()}'>${engineer.getGithub}</a></li>
-                </ul>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+            integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    
+    <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-6" id="header">
+                <h1>My Team</h1>
             </div>
         </div>
-        `;
-    }
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">`
 
-    function generateIntern(intern) {
-        return `
-        <div class = 'card'>
-            <div class = 'header'>
-                <h2> ${intern.getName()}</h2>
-                <h3><i class="fa-sharp fa-solid fa-graduation-cap"></i> Manager</h3>
-            </div>
-            <div>
-                <ul>
-                    <li id = 'employeeId'>${intern.getId()}</li>
-                    <li id = 'managerEmail'>Email: <a href='mailto:${intern.getEmail()}'>${intern.getEmail()}</a></li>
-                    <li> School: ${intern.getSchool()}</li>
-                </ul>
-            </div>
-        </div>
-        `;
-    }
-
-    const html = [];
-
-    html.push(team.filter(employee => employee.getRole() === 'Manager').map(manager => generateManager(manager)));
-
-    html.push(team.filter(employee => employee.getRole() === 'Engineer').map(engineer => generateEngineer(engineer)).join(''));
-
-    html.push(team.filter(employee => employee.getRole() === 'Intern').map(intern => generateIntern(intern)).join(''));
-
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-        
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-            <title>Team Profile</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
-
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-                integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-                crossorigin="anonymous" referrerpolicy="no-referrer" />
-            <link rel="stylesheet" href="style.css" />
-        </head>
-        
-        <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 jumbotron mb-6" id="header">
-                    <h1>My Team</h1>
+const generateMockup = (employeeArray) => {
+    employeeArray.forEach(element => {
+        pageText += `<div class = "col 4">
+            <div class = "card">
+                <div class = "role">
+                    <h2>${element.getName()}</h2>`;
+                        if (element.getRole() === "Manager") {
+                            pageText += `<h3><i class="fa-sharp fa-solid fa-paperclip"></i> Manager</h3>`
+                        } else if (element.getRole() === "Engineer") {
+                            pageText += `<h3><i class="fa-sharp fa-solid fa-wrench-simple"></i> Engineer</h3>`
+                        } else if (element.getRole() === "Intern") {
+                            pageText += `<h3><i class="fa-sharp fa-solid fa-graduation-cap"></i> Manager</h3>`
+                        }
+                pageText += `</div>
+                <div>
+                    <ul>
+                        <li id = "id">${element.getId()}</li>
+                        <li id = "email">Email: <a href = "mailto:${element.getEmail()}">${element.getEmail()}</a></li>`;
+                        if (element.getRole() === "Manager") {
+                            pageText += `<li id = "office">Office number:${element.getOfficeNumber()}</li>`
+                        } else if (element.getRole() === "Engineer") {
+                            pageText += `<li id = "github">GitHub: <a href = "https://github.com/${element.getOfficeNumber()}>${element.getGithub()}</li>`
+                        } else if (element.getRole() === "Intern") {
+                            pageText += `<li> School: ${element.getSchool()}</li>`
+                        }
+                    pageText += `</ul>
                 </div>
             </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 d-flex justify-content-center"></div>
-                ${generateTeam(team)}   
-                </div>
-            </div>
-        </div>       
-    </body>
-    </html>`;
+        </div>`      
+    });
 }
 
+pageText += `</div></div></div></body></html>`;
+
 module.exports = {
-    generateTeam,
+    generateMockup,
 }
